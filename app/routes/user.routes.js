f67aa8b9661d81f0387370e40f0d1e2e3cbf7f16
Aradhana Mavanji Krishnamurthy <aradhana.mavanjikrishnamurthy@in.bosch.com>
2025-12-02 16:47:54 +0530

    updates

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,52 +1,24 @@
 var router = require('express').Router();
 const userController = require('../controllers/user.controller.js');
-const { body, validationResult } = require('express-validator');
-
-// Middleware for async error handling
-const asyncHandler = fn => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-// Middleware for validation
-const validateUser = [
-  body('username').isString().notEmpty(),
-  body('password').isString().notEmpty()
-];
-
-// Middleware for role assignment validation
-const validateRoleAssignment = [
-  body('role').isString().notEmpty()
-];
-
-// Middleware for error tracking
-const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-};
 
 // Retrieve all Users, returns an array of user objects
-router.get('/users', asyncHandler(userController.getAllUsers));
+router.get('/users', userController.getAllUsers);
 
 // Retrieve a single User by id, returns a user object if found
-router.get('/users/:id', asyncHandler(userController.getUserById));
+router.get('/users/:id', userController.getUserById);
 
 // Create a new User, expects user attributes in request body
-router.post('/users', validateUser, asyncHandler(userController.createUser));
+router.post('/users', userController.createUser);
 
-// Update an existing User by id, expects user attributes in request body
-router.put('/users/:id', asyncHandler(userController.updateUser));
+router.put('/users/:id', userController.updateUser);
 
-// Delete a User by id
-router.delete('/users/:id', asyncHandler(userController.deleteUser));
+router.delete('/users/:id', userController.deleteUser);
 
 // Login a User, expects username and password in request body
-router.post('/users/login', validateUser, asyncHandler(userController.verifyUser));
+router.post('/users/login', userController.verifyUser);
 
 // Assign role to a User by id, expects role information in request body
-router.post('/users/:id/role/assign', validateRoleAssignment, asyncHandler(userController.roleAssign));
-
-// Use error handling middleware
-router.use(errorHandler);
+router.post('/users/:id/role/assign', userController.roleAssign);
 
 // End of user routes
 
