7285968bbadfd1bd2c1ab9def914b5be64d41ed9
Aradhana Mavanji Krishnamurthy <aradhana.mavanjikrishnamurthy@in.bosch.com>
2025-12-02 16:42:34 +0530

    updated user

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,31 +1,24 @@
 var router = require('express').Router();
 const userController = require('../controllers/user.controller.js');
 
-// Async error handling middleware
-const asyncHandler = fn => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-};
-
 // Retrieve all Users, returns an array of user objects
-router.get('/users', asyncHandler(userController.getAllUsers));
+router.get('/users', userController.getAllUsers);
 
 // Retrieve a single User by id, returns a user object if found
-router.get('/users/:id', asyncHandler(userController.getUserById));
+router.get('/users/:id', userController.getUserById);
 
 // Create a new User, expects user attributes in request body
-router.post('/users', asyncHandler(userController.createUser));
+router.post('/users', userController.createUser);
 
-// Update an existing User by id, expects user attributes in request body
-router.put('/users/:id', asyncHandler(userController.updateUser));
+router.put('/users/:id', userController.updateUser);
 
-// Delete a User by id
-router.delete('/users/:id', asyncHandler(userController.deleteUser));
+router.delete('/users/:id', userController.deleteUser);
 
 // Login a User, expects username and password in request body
-router.post('/users/login', asyncHandler(userController.verifyUser));
+router.post('/users/login', userController.verifyUser);
 
 // Assign role to a User by id, expects role information in request body
-router.post('/users/:id/role/assign', asyncHandler(userController.roleAssign));
+router.post('/users/:id/role/assign', userController.
 
 // End of user routes
 
