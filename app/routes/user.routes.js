09cef38ad29349819437f4b8bdca76789fa29a75
Aradhana Mavanji Krishnamurthy <aradhana.mavanjikrishnamurthy@in.bosch.com>
2025-12-03 09:33:00 +0530

    updated user routes

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,30 +1,26 @@
-var router = require('express').Router();
-const userController = require('../controllers/user.controller.js');
-const { validateUser, validateRoleAssignment } = require('../middlewares/validation.middleware.js');
-const errorHandler = require('../middlewares/error.middleware.js');
+var router = require("express").Router();
+const userController = require("../controllers/user.controller.js");
 
-// Retrieve all Users, returns an array of user objects
-router.get('/users', userController.getAllUsers);
+// Retrieve all Users
+router.get("/list", userController.getAllUsers);
 
-// Retrieve a single User by id, returns a user object if found
-router.get('/users/:id', userController.getUserById);
+// Retrieve a single User with id
+router.get("/:id/get", userController.getUserById);
 
-// Create a new User, expects user attributes in request body
-router.post('/users', validateUser, userController.createUser);
+// Create a new User    
+router.post("/create", userController.createUser);
 
-router.put('/users/:id', userController.updateUser);
+// Update a User with id
+router.put("/update", userController.updateUser);
 
-router.delete('/users/:id', userController.deleteUser);
+// Delete a User with id
+router.delete("/:id/delete", userController.deleteUser);
 
-// Login a User, expects username and password in request body
-router.post('/users/login', userController.verifyUser);
+// Login a new User    
+router.post("/login", userController.verifyUser);
 
-// Assign role to a User by id, expects role information in request body
-router.post('/users/:id/role/assign', validateRoleAssignment, userController.roleAssign);
+//Role assign to a User
+router.post("/:id/role/assign", userController.roleAssign);
 
-// Error handling middleware
-router.use(errorHandler);
 
-// End of user routes
-
-module.exports = router;
+module.exports = router; 
